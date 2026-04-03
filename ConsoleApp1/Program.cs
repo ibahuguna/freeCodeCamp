ea0450049b5ced1b11304f98c72c057454a667be
unknown <33433097+ibahuguna@users.noreply.github.com>
2026-04-03 19:03:08 +1100

    C# practice from freeCodeCamp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Please enter your name : ");
+string name = Console.ReadLine();
+Console.Write("Enter your age : ");
+string age = Console.ReadLine();
+Console.WriteLine("Hello " + name + "! You are " + age);
+
+Console.ReadLine();
