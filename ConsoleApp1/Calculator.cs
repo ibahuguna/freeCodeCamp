dffddd3de280ef08feb8159fb2d91b624a0b647f
unknown <33433097+ibahuguna@users.noreply.github.com>
2026-04-04 13:26:03 +1100

    Added smart calculator with multiple operations

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+        public void Add(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} + {num2} = " + (num1 + num2));
+        }
+
+        public void Subtract(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} - {num2} = " + (num1 - num2));
+        }
+
+        public void Multiply(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} * {num2} = " + (num1 * num2));
+        }
+
+        public void Divide(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} / {num2} = " + (num1 / num2));
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,38 @@
-﻿Console.Write("Please enter your name : ");
-string name = Console.ReadLine();
-Console.Write("Enter your age : ");
-string age = Console.ReadLine();
-Console.WriteLine("Hello " + name + "! You are " + age);
+﻿using ConsoleApp1;
 
-Console.ReadLine();
+Calculator calculator = new Calculator();
+double num1, num2;
+string operation;
+string cont = "y";
+while (cont == "y" || cont == "Y")
+{
+    Console.Write("Enter a number : ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter another number : ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Select operation (+, -, *, /) : ");
+    operation = Console.ReadLine();
+
+    switch(operation){
+        case "+":
+            calculator.Add(num1, num2);
+            break;
+        case "-":
+            calculator.Subtract(num1, num2);
+            break;
+        case "*":
+            calculator.Multiply(num1, num2);
+            break;
+        case "/":
+            if (num2 != 0)
+                calculator.Divide(num1, num2);
+            else
+                Console.WriteLine("Cannot divide by 0!");
+            break;
+        default:
+            Console.WriteLine("Invalid operation!");
+            break;
+    }
+    Console.Write("Do you want to try again? (y/n) : ");
+    cont = Console.ReadLine();
+}
